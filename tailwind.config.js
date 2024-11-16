050cc881de8274c08c861097c2a0090be3654ae7
curtis <curtisleung361@gmail.com>
2024-11-15 20:09:25 -0800

    hi

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,34 @@ module.exports = {
     extend: {},
   },
   daisyui: {
-    themes: ["retro", "valentine", "lemonade"],
+    themes: [
+      // {
+      //   curtisTheme: {
+      //     primary: "#FF00FF",
+      //     secondary: "#00FFFF",
+      //     accent: "#100FF0",
+      //     "neutral": "#FFFFFF",
+      //     "base-100": "#ff00ff",
+
+      // "--rounded-box": "1rem", // border radius rounded-box utility class, used in card and other large boxes
+      // "--rounded-btn": "1rem", // border radius rounded-btn utility class, used in buttons and similar element
+      // "--rounded-badge": "1.9rem", // border radius rounded-badge utility class, used in badges and similar
+      // "--animation-btn": "0.25s", // duration of animation when you click on button
+      // "--animation-input": "0.2s", // duration of animation for inputs like checkbox, toggle, radio, etc
+      // "--btn-focus-scale": "0.95", // scale transform of button when you focus on it
+      // "--border-btn": "1px", // border width of buttons
+      // "--tab-border": "1px", // border width of tabs
+      // "--tab-radius": "0.5rem", // border radius of tabs
+      //   },
+      // },
+      "retro",
+      "valentine",
+      "lemonade",
+      "forest",
+      "dark",
+      "cupcake",
+      "light",
+    ],
   },
   plugins: [require("daisyui")],
 };
