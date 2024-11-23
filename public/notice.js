ed166d36727d2387f75ef08eadfe9ac8cbd0df0c
curtis <curtisleung361@gmail.com>
2024-11-22 20:15:15 -0800

    my stuff(curtis)

---
+++ public/notice.js
@@ -0,0 +1,6 @@
+// alert("Hello there visitor");
+// alert("It have seemed that you've entered my website");
+// alert("Well, this is a pokemon gallery about only mewtwo vstars");
+// alert("So its not really useful or fun");
+// alert("Anyways, thanks for visiting my website. Hope you have a great time!");
+// alert("Thank you!!!");
